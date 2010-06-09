bd706b438e11b47afc659f5096c16e999f4d383d
Corey M. Hoffstein <corey@hoffstein.com>
2010-06-08 20:13:44 -0400

    Reorganized code

---
+++ app/models/math/optimization/particle_swarm_optimization.rb
@@ -0,0 +1,145 @@
+require 'gsl'
+
+Infinity = 1.0/0
+
+module Math
+  module Optimization
+    class ParticleSwarmOptimization
+      private
+
+      # Our particles
+      class Fly
+        attr_accessor :position, :velocity, :current_fitness
+
+        # Define the feature dimensions, feature limits, et cetera
+        def initialize(feature_dimension, feature_limits,
+                       inertia, cognitive, social)
+          @position = GSL::Vector.alloc(feature_dimension)
+          @velocity = GSL::Vector.alloc(feature_dimension)
+          @velocity.set_all(0)
+
+          @feature_limits = feature_limits
+          
+          @feature_limits.each_index { |i|
+            @position[i] = @feature_limits[i][0] +
+                          (@feature_limits[i][1] - @feature_limits[i][0]) * rand
+          }
+
+          @current_fitness = nil
+          @fitness_max = nil
+          @position_max = nil
+
+          @inertia = inertia
+          @cognitive = cognitive
+          @social = social
+        end
+
+        def current_fitness=(other)
+          @current_fitness = other
+          update_fitness!
+        end
+
+        # update the position based on local and global best, as well as momentum factors
+        def update!(global_best_features)
+          @velocity.each_index { |i|
+            @velocity[i] = @inertia * @velocity[i] +
+              @cognitive * rand * (@position_max[i] - @position[i]) +
+              @social * rand * (global_best_features[i] - @position[i])
+          }
+
+          @position = @position + @velocity
+
+          # ensure we have not exceeded our feature limits
+          0.upto(@position.size-1) { |i|
+            if @position[i] < @feature_limits[i][0]
+              @position[i] = @feature_limits[i][0]
+            elsif @position[i] > @feature_limits[i][1]
+              @position[i] = @feature_limits[i][1]
+            end
+          }
+        end #end update!
+
+        private
+        # update the best local fitness
+        def update_fitness!
+          if @fitness_max.nil?
+            @fitness_max = @current_fitness
+            @position_max = @position
+          else
+            if @current_fitness < @fitness_max
+              @fitness_max = @current_fitness
+              @position_max = @position
+            end
+          end
+        end #end min_max_compare!
+      end #end Fly definition
+
+      public
+
+      # The big function
+      # Inputs:
+      #   population_size: Number of particles you want in the swarm
+      #   feature_dimension: How many features are in the problem
+      #   feature_limits: What range are the features defined in (for initialization only)
+      #   max_iterations: Number of iterations to perform
+      #   inertia: How fast the particle will move in its current direction
+      #   cognitive: How fast the particle moves towards its own personal max
+      #   social: How fast the particle moves towards the global max
+      #   
+      #   Returns the global best fitness and feature set
+
+      DEFAULT_OPTIONS = { :maximum_iterations => 10000,
+                          :tolerance => 1e-16
+                        }
+      def self.optimize_over(population_size, feature_dimension, feature_limits, 
+                             inertia, cognitive, social, options)
+
+        raise "Feature limit definition must be equal to feature size definition" unless feature_dimension == feature_limits.size
+        raise "Block must be provided to optimize features" unless block_given?
+
+        options = Options.set_default_options(options, DEFAULT_OPTIONS)
+
+        # initialize our population
+        @population = Array.new(population_size)
+        @population.map! {
+                Fly.new(feature_dimension, feature_limits, 
+                        inertia, cognitive, social)
+        }
+
+        global_best = nil
+        global_best_features = nil
+
+        iteration = 0
+        begin
+          # find the fitness of each fly and see if it is a local best
+          @population.each { |fly|
+            fly.current_fitness = yield fly.position #should be .clone?
+          }
+
+          # find the best fitness of the current swarm
+          best_fitness_index = GSL::Vector[*@population.map {
+                |fly| fly.current_fitness }].sort_largest_index(1)
+          best_fitness = @population[best_fitness_index].current_fitness
+
+          # check to see if it is a global best
+          if global_best.nil?
+            global_best = best_fitness
+            global_best_features = @population[best_fitness_index].position
+          elsif best_fitness < global_best
+            global_best = best_fitness
+            global_best_features = @population[best_fitness_index].position
+          end
+
+          # update each fly with the global best feature set
+          @population.each { |fly|
+            fly.update!(global_best_features)
+          }
+
+          iteration = iteration + 1
+        end while iteration < options[:max_iterations] && 
+
+        return [global_best, global_best_features]
+      end #end optimize_over
+    end #end ParticleSwarmOptimization definition
+  end
+end
---
+++ app/models/math/optimization/quadratic_programming.rb
@@ -0,0 +1,123 @@
+# Author::    Corey M. Hoffstein  (corey@hoffstein.com)
+# Copyright:: Copyright (c) 2010
+# License::   Distributes under the same terms as Ruby
+
+require 'gsl'
+
+Inf = 1.0/0
+
+# Function solves:
+#
+# min QP(x) = 0.5*x'Hx + f'x
+#  x
+#
+# s.t.	a'x = b
+#       LB[i] <= x[i] <= UB[i] for all i=1..n
+#
+# Using the Generalized SMO algorithm outline in 
+#    S. Keerthi, E.G.Gilbert. Convergence of a Generalized SMO Algorithm for SVM Classifier Design. 
+#    Technical Report CD-00-01, Control Division, Dept. of Mechanical and Production Engineering, 
+#    National University of Singapore, 2000. http://citeseer.ist.psu.edu/keerthi00convergence.html
+#
+# x is the initial guess
+
+module Math
+  module Optimization
+    module QuadraticProgramming
+    
+      DEFAULT_OPTIONS = {
+        :max_iterations   => 10000,
+        :tolerance        => 1e-32,
+      }
+    
+      def self.generalized_smo(h, f, a, b, lb, ub, x, options={})
+        ret = {
+          :x => x,
+          :exit_code => 0,
+          :qp => nil
+        }
+     
+        # set up our default algorithm options
+        options = Options::set_default_options(options, DEFAULT_OPTIONS)
+    
+        n = ret[:x].length
+    
+        # The gradient in Hx + f
+        gradient = h*ret[:x] + f
+        diag_H = h.diagonal
+	
+        iterations = 0
+        current_tolerance = Inf
+        while ret[:exit_code] == 0
+          iterations = iterations + 1
+	    
+          minF_up = Inf; maxF_low = -Inf
+          u = 0; v = 0
+	    
+          f = gradient / a
+	    
+          (0...n).each { |i|
+            if lb[i] < ret[:x][i] && ret[:x][i] < ub[i]
+              if minF_up > f[i]
+                minF_up = f[i]; u = i
+              end
+              if maxF_low < f[i]
+                maxF_low = f[i]; v = i
+              end
+            elsif (a[i] > 0 && ret[:x][i] == lb[i]) || (a[i] < 0 && ret[:x][i] == ub[i])
+              if minF_up > f[i]
+                minF_up = f[i]; u = i
+              end
+            elsif (a[i] > 0 && ret[:x][i] == ub[i]) || (a[i] < 0 && ret[:x][i] == lb[i])
+              if maxF_low < f[i]
+                maxF_low = f[i]; v = i
+              end
+            end
+	        
+            tolerance = (maxF_low - minF_up).abs
+            if tolerance > options[:tolerance]
+              col_u = h.column(u)
+              col_v = h.column(v)
+	            
+              if a[u] > 0
+                tau_lb = (lb[u]-ret[:x][u])*a[u]
+                tau_ub = (ub[u]-ret[:x][u])*a[u]
+              else
+                tau_ub = (lb[u]-ret[:x][u])*a[u]
+                tau_lb = (ub[u]-ret[:x][u])*a[u]
+              end
+	            
+              if a[v] > 0
+                tau_lb = [tau_lb, (ret[:x][v]-ub[v])*a[v]].max
+                tau_ub = [tau_ub, (ret[:x][v]-lb[v])*a[v]].min
+              else
+                tau_lb = [tau_lb, (ret[:x][v]-lb[v])*a[v]].max
+                tau_ub = [tau_ub, (ret[:x][v]-ub[v])*a[v]].min
+              end
+	            
+              tau = (gradient[v]/a[v] - gradient[u]/a[u]) /
+                ((diag_H[u]/(a[u]**2) + diag_H[v])/(a[v]**2) - 2*col_u[v]/(a[u]*a[v]))
+	            
+              tau = [[tau, tau_lb].max, tau_ub].min
+	            
+              ret[:x][u] += tau/a[u]
+              ret[:x][v] -= tau/a[v]
+	            
+              gradient = gradient + col_u*tau/a[u] - col_v*tau/a[v]
+            else
+              ret[:exit_code] = :tolerance
+            end
+          }
+	    
+          if iterations > options[:max_iterations]
+            ret[:exit_code] = :maximum_iterations
+          end
+        end
+    
+        ret[:qp] = 0.5 * ret[:x].transpose * h * ret[:x] + f.transpose * ret[:x]
+
+        return ret
+      end
+    end
+  end
+end
