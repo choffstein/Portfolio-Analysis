86f6698310dfbe9756466a02b2e78619ced306db
Corey M. Hoffstein <corey@hoffstein.com>
2010-06-18 00:04:34 -0400

    Edited readme

--- app/controllers/data_controller.rb
+++
@@ -1,9 +0,0 @@
-class DataController < ApplicationController
-  def upload
-    if request.post?
-      data_file = DataFile.new(params[:upload])
-      render :text => "#{data_file.dates}, #{data_file.prices}, #{data_file.volume}"
-    end
-  end
-
-end
