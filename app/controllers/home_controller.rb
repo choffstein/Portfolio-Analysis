2e5541b8187a013e754f3f845cb3d1291d24c21d
Corey M. Hoffstein <corey@hoffstein.com>
2010-06-18 10:33:21 -0400

    Changed folders

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,23 +13,59 @@ class HomeController < ApplicationController
     end
   end
 
+  def monte_carlo
+    respond_to do |wants|
+      wants.js {
+        if session[:portfolio].nil?
+          render :text => "Please upload portfolio first"
+        else
+          state = Portfolio::State.new({:tickers => session[:portfolio].tickers,
+                          :number_of_shares => session[:portfolio].shares})
+          sliced_state = state.slice(state.dates.size - 1000) # 5 years
+          mc_series = sliced_state.monte_carlo
+
+          g = Gruff::Line.new(mc_series.size2)
+          g.title = "Monte Carlo Simulation"
+          0.upto(mc_series.size1-1) { |i|
+            g.data("Series #{i}", mc_series.row(i).to_a)
+          }
+
+          send_data(g.to_blob,
+                    :disposition => 'inline',
+                    :type => 'image/png',
+                    :filename => "mc_simulation.png")
+        end
+      }
+    end
+  end
+
   def risk_analysis
-    if request.post?
-      Rails.logger.info(params)
-      data_file = DataFile.new(params[:upload])
+    respond_to do |wants|
+      wants.js {
+        if session[:portfolio].nil?
+          render :text => "Please upload portfolio first"
+        else
+          t = Time.now
+          state = Portfolio::State.new({:tickers => session[:portfolio].tickers,
+              :number_of_shares => session[:portfolio].shares})
+          sliced_state = state.slice(state.dates.size - 1000) # 5 years
+          composite_risk = Portfolio::Risk::ValueAtRisk.composite_risk(sliced_state)
 
-      t = Time.now
-      state = Portfolio::State.new({:tickers => date_file.tickers,
-                                    :number_of_shares => date_file.shares})
-      sliced_state = state.slice(state.dates.size - 1000) # 5 years
-      composite_risk = Portfolio::Risk::ValueAtRisk.composite_risk(sliced_state)
+          total = Time.now - t
 
-      total = Time.now - t
-    
-      render :text => "#{composite_risk} (computed in #{total}s)"
+          render :text => "#{composite_risk} (computed in #{total}s)"
+        end
+      }
     end
   end
 
   def state_test
+    @portfolio = session[:portfolio]
+    Rails.logger.info(@portfolio.tickers.zip(@portfolio.shares)) unless @portfolio.nil?
+
+    if request.post?
+      Rails.logger.info(params)
+      session[:portfolio] = DataFile.new(params[:portfolio])
+    end
   end
 end
