21d452fdeb638ab5b0f694b88813709e79a4dd32
Corey M. Hoffstein <corey@hoffstein.com>
2010-07-06 17:49:01 -0400

    Removed clusterer dependence

--- config/environment.rb
+++ config/environment.rb
@@ -44,7 +44,6 @@ Rails::Initializer.run do |config|
   config.gem 'rubystats'
   config.gem 'statistics2'
   config.gem 'third_base'
-  config.gem 'clusterer'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
