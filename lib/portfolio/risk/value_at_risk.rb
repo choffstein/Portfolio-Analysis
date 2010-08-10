e9ed5acca8240f2d6a15e156915cfcfc997968d7
Corey M. Hoffstein <corey@hoffstein.com>
2010-08-10 08:45:40 -0400

    Added monte-carlo technique

--- lib/portfolio/risk/value_at_risk.rb
+++ lib/portfolio/risk/value_at_risk.rb
@@ -1,14 +1,42 @@
 module Portfolio
   module Risk
 
-    Standard_Normal_Distribution = Rubystats::NormalDistribution.new(0.0, 1.0)
-
     module ValueAtRisk
+
+      def self.monte_carlo(log_returns, days = 1, confidence_level = 0.95)
+        block_size = 25
+        while days % block_size != 0
+          block_size = block_size - 1
+        end
+
+        series = Statistics::Bootstrap.block_bootstrap(log_returns, days, block_size, 2500, 1)
+
+        n = series.size1
+        returns = GSL::Vector.alloc(n)
+
+        n.times { |i|
+          row = series.row(i)
+          values = row.to_a.map { |i| log_returns.get(i.to_i, block_size).to_a }.flatten
+          v = GSL::Vector[*values]
+          cumulative = v.cumsum
+          #check if there was ever any bankruptcy (within cumulative)?
+          returns[i] = Math.exp(cumulative[-1]) - 1.0
+        }
+
+        percentile_index = (n*(1.0 - confidence_level)).floor
+        returns = returns.sort_smallest(percentile_index)
+        
+        var = -returns[-1]
+        cvar = -returns.mean
+
+        return { :var => var, :cvar => cvar }
+      end
+
       # Cornish Fisher Value-at-Risk uses a boot-strapping method
       # to more appropriately determine the summary statistics for
       # a series of excess log-returns, and then adjusts the standard
       # value at risk statistic for skew and kurtosis factors
-      # 
+      #
       # See http://papers.ssrn.com/sol3/papers.cfm?abstract_id=1535933
       def self.cornish_fisher(log_returns, days = 1, confidence_level=0.99)
         block_size = 25
@@ -41,18 +69,18 @@ module Portfolio
 
           # see http://www.riskglossary.com/link/cornish_fisher.htm
           za = z +
-               (1.0/6.0)*(z**2 - 1.0)*skewness +
-               (1.0/24.0)*(z**3 - 3.0*z)*kurtosis -
-               (1.0/36.0)*(2.0*(z**3) - 5.0*z)*(skewness**2)
+            (1.0/6.0)*(z**2 - 1.0)*skewness +
+            (1.0/24.0)*(z**3 - 3.0*z)*kurtosis -
+            (1.0/36.0)*(2.0*(z**3) - 5.0*z)*(skewness**2)
 
           stddev = Math.sqrt(variance)
 
           left_tail_cutoff = mean - za * stddev
  
           total_left_tail_prob = 0.5 * (1.0 +
-                  Math.erf((left_tail_cutoff - mean) / Math.sqrt(2.0*variance)))
+              Math.erf((left_tail_cutoff - mean) / Math.sqrt(2.0*variance)))
           k = (1.0 / Math.sqrt(2.0*Math::PI*variance)) / total_left_tail_prob
-          f = GSL::Function.alloc{ |x|
+          f = GSL::Function.alloc { |x|
             x * k * Math.exp(-((x-mean)**2) / (2.0*variance))
           }
 
@@ -66,7 +94,9 @@ module Portfolio
       end
 
       # See "HotSpots and Hedges" paper by Litterman, Goldman Sachs
-      def self.composite_risk(portfolio_state, days = 1)
+      def self.composite_risk(portfolio_state, days = 1, method=:monte_carlo)
+        raise "Must choose valid risk method" unless [:cornish_fisher, :monte_carlo].include?(method)
+
         Status.info('Computing composite risk')
         # log-returns are row oriented
         returns = portfolio_state.log_returns
@@ -79,7 +109,7 @@ module Portfolio
         last_date = portfolio_state.dates[-1]
         n.times { |i|
           total_holdings[i] = portfolio_state.number_of_shares[i] *
-            portfolio_state.time_series[portfolio_state.tickers[i]][last_date]
+          portfolio_state.time_series[portfolio_state.tickers[i]][last_date]
         }
         
         total_portfolio_value = portfolio_state.current_portfolio_value
@@ -89,7 +119,7 @@ module Portfolio
         beta = total_portfolio_value * variance / portfolio_variance
 
         Status.info("Computing Portfolio VaR")
-        portfolio_vars = cornish_fisher(portfolio_state.to_log_returns, days)
+        portfolio_vars = send(method, portfolio_state.to_log_returns, days)
         portfolio_var = portfolio_vars[:var] #* total_portfolio_value
         portfolio_cvar = portfolio_vars[:cvar] #* total_portfolio_value
 
@@ -97,7 +127,7 @@ module Portfolio
         cvars = GSL::Vector.alloc(n)
         n.times { |i|
           Status.info("Computing VaR for #{portfolio_state.tickers[i]} (#{i+1}/#{n})")
-          value_at_risk = cornish_fisher(returns.row(i), days)
+          value_at_risk = send(method, returns.row(i), days)
           vars[i] =  value_at_risk[:var]
           cvars[i] = value_at_risk[:cvar]
         }
